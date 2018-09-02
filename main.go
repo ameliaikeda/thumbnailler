0647f03f8231e7ca27bafd9132dd772033f232d7
Amelia Ikeda <ameliaikeda@noreply.users.github.com>
2018-09-02 19:09:39 +0100

    Allow deleting a file based on a search (but really, really fast)

--- main.go
+++ main.go
@@ -13,6 +13,9 @@ import (
 var (
 	source, destination    string
 	width, height, quality int
+
+	del    bool
+	search string
 )
 
 func init() {
@@ -21,11 +24,33 @@ func init() {
 	flag.IntVar(&width, "width", 300, "the width of generated thumbnails. aspect ratio is preserved.")
 	flag.IntVar(&height, "height", 300, "the height of generated thumbnails. aspect ratio is preserved.")
 	flag.IntVar(&quality, "quality", 80, "the defined JPEG quality for output.")
+	flag.BoolVar(&del, "delete", false, "switches to delete mode on a search")
+	flag.StringVar(&search, "search", "", "a path match (non-regex) to use for fast file deletion")
 
 	flag.Parse()
 }
 
 func main() {
+	if del {
+		if search == "" {
+			fmt.Fprintln(os.Stderr, "error: search is required when deleting\n")
+			flag.Usage()
+
+			os.Exit(2)
+		}
+
+		processor.SetSearch(search)
+
+		err := walk.Walk(processor.Expand(source), processor.Delete)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "walk error: %+v", err)
+
+			os.Exit(1)
+		}
+
+		return
+	}
+
 	if destination == "" {
 		fmt.Fprintln(os.Stderr, "error: dst must be set\n")
 		flag.Usage()
---
+++ processor/deleter.go
@@ -0,0 +1,34 @@
+package processor
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+var search string
+
+// Delete will delete all files that match a specified path.
+func Delete(path string, info os.FileInfo, err error) error {
+	p, err := filepath.Abs(path)
+	if err != nil {
+		return err
+	}
+
+	if strings.Contains(p, search) {
+
+		fmt.Printf("delete: %s\n", p)
+
+		return os.Remove(p)
+	}
+
+	fmt.Printf("skip  : %s\n", p)
+
+	return nil
+}
+
+// SetSearch sets the match string to use for strings.Contains.
+func SetSearch(s string) {
+	search = s
+}
